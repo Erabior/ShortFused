54273ec55de3bf1fff1512429c57229786048991
Erabior <magnusjtaylor21@gmail.com>
2023-12-10 14:50:47 -0500

    Initial Release

---
+++ Patches/AutoEngineerPATCH.cs
@@ -0,0 +1,35 @@
+﻿using Game;
+using HarmonyLib;
+using Model.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConductor.Patches
+{
+    [HarmonyPatch(typeof(AutoEngineerPlanner), "Search")]
+    internal class AutoEngineerPATCH
+    {
+        [HarmonyPostfix]
+        static void shortFusePatch(ref float? nextFlareDistance)
+        {
+            if (nextFlareDistance.HasValue)
+            {
+                nextFlareDistance += 16;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(FlareManager), "NearbyFlares")]
+    internal class FlareRadiusPATCH
+    {
+        [HarmonyPostfix]
+        static void ReduceRadius(ref float radius)
+        {
+            radius = 10f; 
+        }
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BetterConductor.Patches;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConductor
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BetterConductor: BaseUnityPlugin
+    {
+        private const string modGUID = "Erabior.BetterConductor";
+
+        private const string modName = "Better Conductor Mod";
+
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static BetterConductor Instance;
+
+        internal ManualLogSource mls;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Loaded Better Conductor");
+
+            harmony.PatchAll(typeof(BetterConductor));
+            harmony.PatchAll(typeof(AutoEngineerPATCH));
+
+        }
+    }
+}
